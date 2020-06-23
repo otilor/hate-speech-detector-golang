e808c5724eac7422751dd26abe877afb1e336893
GabielFemi <gabrielfemi799@gmail.com>
2020-06-23 18:15:38 +0100

    Add function to handle / route and also create a server using http.NewServeMux()

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/gorilla/mux"
+	"log"
+)
 
-func main() {
+func main() 
+	router := mux.NewRouter()
+	router.HandleFunc("/", hateSpeech.Index)
 	fmt.Println("Hate Speech Detector Application using Go")
+	server := http.Server {
+		Addr: "127.0.0.1:8000",
+		Handler: router,
+	}
+
+	log.Fatalln(server.ListenAndServe())
 }
