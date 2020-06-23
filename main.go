bb452c0db4af385671a1875cf95198e5711cbda0
GabielFemi <gabrielfemi799@gmail.com>
2020-06-23 18:22:45 +0100

    Add pointer referencing to http.Server object

--- main.go
+++ main.go
@@ -2,15 +2,18 @@ package main
 
 import (
 	"fmt"
+	"github.com/GabielFemi/hate-speech-detector-golang/hateSpeech"
 	"github.com/gorilla/mux"
 	"log"
+	"net/http"
 )
 
 func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/", hateSpeech.Index)
 	fmt.Println("Hate Speech Detector Application using Go")
-	server := http.Server{
+
+	server := &http.Server{
 		Addr:    "127.0.0.1:8000",
 		Handler: router,
 	}
