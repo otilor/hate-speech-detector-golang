6129636cb558adb7b87a7bc76a71847b40c841ff
GabielFemi <gabrielfemi799@gmail.com>
2020-06-25 10:05:47 +0100

    Handle assets directory and serve it using router.PathPrefix

--- main.go
+++ main.go
@@ -10,9 +10,17 @@ import (
 
 func main() {
 	router := mux.NewRouter()
+
+	// Handle assets directory
+	router.PathPrefix("/assets").Handler(http.StripPrefix("/assets/", http.FileServer(http.Dir("assets"))))
+
+	// Handle routes
 	router.HandleFunc("/", hateSpeech.Index)
+
+	// Console message
 	fmt.Println("Hate Speech Detector Application using Go")
 
+	// Initialize server that creates an instance of http.Server
 	server := &http.Server{
 		Addr:    "127.0.0.1:8000",
 		Handler: router,
