ecd39530edd3cf04591fe26c07bd4b31ff7beba6
GabielFemi <gabrielfemi799@gmail.com>
2020-06-25 09:56:46 +0100

    Add method to handle rendering of HTML, add default page also.

---
+++ hateSpeech/view.go
@@ -0,0 +1,16 @@
+package hateSpeech
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+)
+
+func render (w http.ResponseWriter, tmpl string, r *http.Request) {
+	t, err := template.ParseFiles(tmpl)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	err = t.Execute(w, nil)
+}
