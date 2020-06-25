22e54adb932c9c075bfa8ae792aa52a85a6adc11
GabielFemi <gabrielfemi799@gmail.com>
2020-06-25 09:59:17 +0100

    Handle possible error when parsing template file

--- hateSpeech/view.go
+++ hateSpeech/view.go
@@ -2,15 +2,11 @@ package hateSpeech
 
 import (
 	"html/template"
-	"log"
 	"net/http"
 )
 
 func render (w http.ResponseWriter, tmpl string, r *http.Request) {
 	t, err := template.ParseFiles(tmpl)
-	if err != nil {
-		log.Fatalln(err)
-	}
-
+	handleError(err)
 	err = t.Execute(w, nil)
 }
